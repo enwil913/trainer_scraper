b724baea77b26fe452fce98ab1324a51c6bdb03c
enwil913 <enwil913@gmail.com>
2023-11-17 16:15:19 +0800

    202231117

--- pages/api/trainer.ts
+++ pages/api/trainer.ts
@@ -17,12 +17,12 @@ export default async function getTrainers(
   res: NextApiResponse
 ) {
   try {
-    const { data } = await axios.get(getUrl);
+    const data = await axios.get(getUrl);
     
     const dom = new JSDOM(data);
     
     console.log(data);
-    
+
     //filter data here
     const trainersElement: HTMLCollectionOf<Element> =
       dom.window.document.querySelectorAll(".trs");
