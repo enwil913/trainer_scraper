9fed27a18eb457ca38fd74ff3c55c9f02b9f25ad
enwil913 <enwil913@gmail.com>
2023-11-21 12:42:06 +0800

    202231121

--- pages/api/trainer.ts
+++ pages/api/trainer.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 import { JSDOM } from "jsdom";
 
 
-const getUrl = `https://racing.on.cc/racing/ifo/current/rjifob0001x0.html`;
+//for meta race date meta data
+const localResultURL = "https://racing.on.cc/racing/rat/current/rjratg0001x0.html"
+//for race card list
+const cardListURL = "https://racing.on.cc/racing/ifo/current/rjifob0001x0.html"
 
 
 function removeConsecutiveBlanks(str: string) {
@@ -68,51 +71,34 @@ export default async function getTrainers(
   res: NextApiResponse
 ) {
     try {
-        const { data } = await axios.get(getUrl, {
+        //get race dates
+        const raceDate = await axios.get(localResultURL, {
+            responseType: 'arraybuffer',
+            transformResponse: [function (data) {
+              const iconv = require('iconv-lite')
+              return iconv.decode(Buffer.from(data), 'big5')
+            }]
+          });
+        const dateData = raceDate.data;    
+        const dom = new JSDOM(dateData);
+        const dateSelect : HTMLCollectionOf<Element> 
+            = dom.window.document.querySelectorAll('select');
+
+        const dates = Array.from(dateSelect, (date) => {
+            console.log(date.innerHTML)
+        });
+
+        //get card list
+        const cardList = await axios.get(cardListURL, {
             responseType: 'arraybuffer',
             transformResponse: [function (data) {
               const iconv = require('iconv-lite')
               return iconv.decode(Buffer.from(data), 'big5')
             }]
           });    
+        const trainersResult = getTrainersList(cardList.data);
 
-        // const dom = new JSDOM(data);
-        // const trainersTable : HTMLCollectionOf<Element> 
-        //     = dom.window.document.querySelectorAll(".stable tr");
-    
-        // // const testTD : HTMLCollectionOf<Element> 
-        // // = dom.window.document.querySelectorAll(".stable tr td");
-    
-        // const trainers = Array.from(trainersTable, (trainer) => {
-        //     // console.log(trainer.innerHTML)
-        //     const trainerText = removeConsecutiveBlanks(trainer.innerHTML);
-        //     const trainerInfoArr = trainerText.split("<td>");
-        //     let trainerName = 'No Trainer';
-        //     let trainerWin = 'No Trainer';
-        //     let trainerHistory = [];
-    
-        //     if (trainerInfoArr[5] !== undefined) {
-        //         const firstIndex = trainerInfoArr[5].indexOf(">")
-        //         const nextIndex = trainerInfoArr[5].indexOf("<", firstIndex + 1)
-        //         trainerName = trainerInfoArr[5].substring(firstIndex + 1, nextIndex);
-        //         if (trainerInfoArr[1] !== undefined) {
-        //             const nextIndex = trainerInfoArr[1].indexOf("<")
-        //             trainerWin = trainerInfoArr[1].substring(0, nextIndex);
-        //         }
-        //     }
-        //     return { //init the trainer data
-        //         trainerName,
-        //         trainerWin,
-        //         trainerHistory,
-        //     };
-        // });
-        // //fitler the trainer and fill-in trainer details
-        // const trainersResult = trainers.filter(checkTrainName);
-        // for (var t in trainersResult) {
-        //     trainersResult[t].trainerHistory = [1, 2, 3]
-        // }
-       
-        const trainersResult = getTrainersList(data);
+
         res.status(200).json({trainerData: trainersResult});
 
     } catch (error) {
