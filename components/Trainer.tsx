01e693b871b297e23421a50220c6bd3ef69ded40
enwil913 <enwil913@gmail.com>
2023-11-21 12:00:39 +0800

    202231121

--- components/Trainer.tsx
+++ components/Trainer.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 
 const Trainer = ({ trainer }) => {
+    const historyList = trainer.trainerHistory.map((win: any) => "|" + win + "|")
+
     return (
         <div key={trainer.trainerName} className='trainer'>
             <h3>{trainer.trainerName}</h3>
             <p>{trainer.trainerWin}</p>  
-            <p>Win History</p>  
+            <p>{historyList}</p>  
       </div>
     )
   }
