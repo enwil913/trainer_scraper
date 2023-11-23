74f0e84a4310e6afba139f1cda2fa6e5461db9fc
enwil913 <enwil913@gmail.com>
2023-11-23 15:17:19 +0800

    202231121

--- components/Trainer.tsx
+++ components/Trainer.tsx
@@ -7,12 +7,11 @@ const Trainer = ({ trainer }) => {
     return (
         <div key={trainer.trainerName} className='trainer'>
             <h3>
-                {trainer.trainerName}
-                ({trainer.trainerShortName})
-                Total Win: {trainer.trainerWin} 
-                ConsecutiveLoss: {consecutiveLoss}  
+                {trainer.trainerName} ({trainer.trainerShortName}) 
+                馬房 (冠): {trainer.trainerWin} 
+                連輸: {consecutiveLoss}  
             </h3>
-            <p>{historyList}</p>  
+            <p>羸馬記錄(左至右): {historyList}</p>  
             <p>{trainer.log}</p>
         </div>
     )
