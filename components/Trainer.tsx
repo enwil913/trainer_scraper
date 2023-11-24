01607d92bd23eb75c895223950704b10c3e75626
enwil913 <enwil913@gmail.com>
2023-11-24 13:19:49 +0800

    202231121

--- components/Trainer.tsx
+++ components/Trainer.tsx
@@ -11,9 +11,9 @@ const Trainer = ({ trainer }) => {
                 <div>{trainer.trainerName} {trainerShortName}</div> 
                 <div>馬評: <span style={(trainer.trainerConsecutiveLoss >= 3) ? {color: 'red'} : 
                 (trainer.trainerConsecutiveLoss == 2) ? {color: "royalblue"} : {}}>{trainer.log}</span></div>
-                <div>總頭馬: {trainer.trainerWin}</div> 
             </h4>
             <p>
+                <div>總頭馬: {trainer.trainerWin}</div> 
                 <div>連輸: {trainer.trainerConsecutiveLoss}</div>  
                 <div>近十場頭馬(左至右): {historyList}</div>
             </p>  
